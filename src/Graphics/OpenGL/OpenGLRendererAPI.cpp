cf6e446d1a1d050820496a53ebbfebffbc8fd20d
Sparky <ryanduncan114@gmail.com>
2024-09-15 09:57:31 +1000

    Removed OpenGL code from RendererAPI.

--- src/Graphics/OpenGL/OpenGLRendererAPI.cpp
+++ src/Graphics/OpenGL/OpenGLRendererAPI.cpp
@@ -8,6 +8,8 @@ namespace CrazyEngine
 {
     void OpenGLRendererAPI::Initialise()
     {
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     }
 
     void OpenGLRendererAPI::Terminate()
